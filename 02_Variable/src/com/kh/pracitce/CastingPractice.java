2ffc1325bab39240516bbd509defd8e2a12f9895
john_mayer <130232731+mayerjogn@users.noreply.github.com>
2023-05-18 02:56:34 +0900

    Update CastingPractice.java

--- 02_Variable/src/com/kh/pracitce/CastingPractice.java
+++ 02_Variable/src/com/kh/pracitce/CastingPractice.java
@@ -14,16 +14,41 @@ public class CastingPractice {
 	}
 	public void method1() {
 		Scanner sc = new Scanner(System.in);
-				
+		
 		System.out.printf("문자 : ");
 		String name = sc.next();
+				
+		//흠 "여기엔 뭘넣어야 입력하는 문자대로 바뀔까요 unicode : "
+		char ch = name.charAt(0);
+		System.out.println(" unicode : " + (int)ch); 
+		System.out.println(" unicode : " + ((int)ch+1) );//이것도 확실 맞는지 모르겠어요 이렇게 하면 유니코드 마지막에 그냥 +1만 되는거 아닌가요..
 		
-		int num1 = 'A';
-		int num2 = 'B';
+		//System.out.println();
+		//char ch2 = name.charAt(0);
+		//System.out.print("unicode : " + (int)ch2 );
+		
+		//char ch1 = name.charAt(0);
+		
+		//System.out.print("unicode :  ");
+		
+		//char ch2 = name.charAt(0);
 		
 		
-		System.out.println("A unicode : " + num1);
-		System.out.println("B unicode : " + num2);
+		
+		//System.out.printf("%s :  " +(int)ch1  );
+		//System.out.println();
+		//System.out.printf("%s :  " +(int)ch2 );
+		
+		
+		//char ch1 = name.charAt(0);
+		//char ch2 = name.charAt(0);
+		//int num1 = 'A';
+		//int num2 = 'B';
+			
+		//System.out.println("A unicode : " + (int) 'A');
+		//System.out.println("B unicode : " + (int) 'B');
+		//System.out.println("A unicode : " + 'A');
+		//System.out.println("B unicode : " + 'B');
 		
 	}
 	
@@ -39,21 +64,23 @@ public class CastingPractice {
 		
 		System.out.print("수학 : ");
 		int c = sc.nextInt();
-		
-//		int iNum = 115;
-//		double bNum = 38.33;
-		
-		
-		
-//		System.out.printf("총점 : " + iNum);
-//	    System.out.printf("\n평균 : " + bNum);
-		
+				
 		float f1 =(a+b+c);
 		float f2 = (f1/3);
 		
-		System.out.printf("총점 : " + f1);
-		System.out.printf("\n평균 : " + f2);
-	    //저는 이거처럼 계산해서 소수점 2자리까지만 나타내고 싶은데  &.f\를 어디다 넣어야 하는지 아니면 코드를 다시짜야 하는지 잘 모르겠어요
+		
+		System.out.printf("총점 : %.f\n " + f1);
+		System.out.printf("평균 : %.2f\n " + f2);
+		
+	    //암만 생각해봐도 소수점 자리 자르는건 이거같은데  총점이랑 평균 나올때 이런 오류가떠요
+		/*Exception in thread "main" java.util.UnknownFormatConversionException: Conversion = '.'
+		at java.base/java.util.Formatter.parse(Formatter.java:2750)
+		at java.base/java.util.Formatter.format(Formatter.java:2671)
+		at java.base/java.io.PrintStream.format(PrintStream.java:1209)
+		at java.base/java.io.PrintStream.printf(PrintStream.java:1105)
+		at com.kh.pracitce.CastingPractice.method2(CastingPractice.java:64)
+		at com.kh.pracitce.CastingPractice.main(CastingPractice.java:12)*/
+
 	}
 	
 }
