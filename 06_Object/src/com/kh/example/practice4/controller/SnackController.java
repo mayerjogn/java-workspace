38671ae27ee0891f63b95756cda7e559a7d3a709
johnmayer <130232731+mayerjogn@users.noreply.github.com>
2023-05-26 15:06:39 +0900

    돌대가리련

--- 06_Object/src/com/kh/example/practice4/controller/SnackController.java
+++ 06_Object/src/com/kh/example/practice4/controller/SnackController.java
@@ -2,35 +2,41 @@ package com.kh.example.practice4.controller;
 
 import com.kh.example.practice4.model.Snack;
 
-/*
- * controller : 사용자가 화면에서 요청한 부분을 처리하는 기능 담당
- * 이때 처리 후에 결과를 여기서 출력하는게 아니라 결과값을 
- * 다시 view(ex:run 애플리케이션 등등) 에
- * 반환하여 view에 처리하도록 해야함 
- * */
-
 public class SnackController {
-
-	private Snack s = new Snack();
 	
+	// 멤버변수 -> 보통 모델로 만들어놓은 클래스!
+	Snack snack = new Snack();
+	
+	// 생성자
 	public SnackController() {}
 	
-	// 데이터를 setter를 이용해 저장하고 저장 되었다는 
-	// 결과를 반환하는 메서드
-	
-	public boolean saveDate(Snack s) {
+	// 메서드
+	// 데이터를 setter를 이용해 저장하고 
+	// 저장되었다는 결과를 반환하는 메서드
+	public boolean saveData(Snack viewSnack) {
 		
-		this.s.setKind(s.getKind());
-		this.s.setName(s.getName());
-		this.s.setFlavor(s.getFlavor());
-		this.s.setNumOf(s.getNumOf());
-		this.s.setPrice(s.getPrice());
+		// snack은 controller에서 만든 임시 데이터 창고		
+		// viewSnack은 Application에서 데이터를 담은 임시 데이터창고
+		
+//		String kind = viewSnack.getKind();
+//		snack.setKind(kind); 아래꺼에 익숙해져보자 
+		
+		snack.setKind(viewSnack.getKind());
+		snack.setName(viewSnack.getName());
+		snack.setFlavor(viewSnack.getFlavor());
+		snack.setNumOf(viewSnack.getNumOf());
+		snack.setPrice(viewSnack.getPrice());
 		
 		return true;
-	}
+	}		
+	//controller에 저장된 데이터를 반환하는 메서드
 	
-	//저장된 데이터를 반환하는 메서드
-	public Snack confirmData() {
-		return this.s;
+	public Snack confirmData() { //더블체크 하는곳
+		return snack;
+		//return this.snack; 위랑 같음
 	}
 }
+
+
+
+
